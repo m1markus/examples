db12d9510062845af8aa06b72296d413ceb6befd
Markus <mmm7@bluewin.ch>
2017-07-17 00:18:02 +0200

    ldapex1: initial import

---
+++ ldapex1/src/main/java/ch/m1m/ldap/MainLdapExample.java
@@ -0,0 +1,107 @@
+package ch.m1m.ldap;
+
+import com.unboundid.ldap.listener.InMemoryDirectoryServer;
+import com.unboundid.ldap.listener.InMemoryDirectoryServerConfig;
+import com.unboundid.ldap.listener.InMemoryListenerConfig;
+import com.unboundid.ldap.sdk.LDAPConnection;
+import com.unboundid.ldap.sdk.LDAPException;
+import com.unboundid.ldap.sdk.LDAPSearchException;
+import com.unboundid.ldap.sdk.SearchResult;
+import com.unboundid.ldap.sdk.SearchResultEntry;
+import com.unboundid.ldap.sdk.SearchScope;
+
+
+// https://www.ldap.com/unboundid-ldap-sdk-for-java
+// https://docs.ldap.com/ldap-sdk/docs/index.html
+// https://docs.ldap.com/ldap-sdk/docs/ldapsdk-faq.html#search
+
+/**
+ * Created by mue on 15.07.17.
+ */
+public class MainLdapExample {
+	
+	static final int LDAP_PORT = 3389;
+	
+	
+	public static void doSimpleQuery(LDAPConnection conn) throws LDAPSearchException {
+		
+		System.out.println("Executing new simpleQuery...");
+		
+		// SearchResultEntry entry = conn.getEntry("dc=example,dc=com");
+		// System.out.println(entry.toString());
+		
+		//SearchResult searchResult = conn.search("dc=example,dc=com", SearchScope.SUB, "(cn=*)");
+		// SearchResult searchResult = conn.search("dc=example,dc=com", SearchScope.SUB, "(cn=Markus Mueller)");
+		
+		SearchResult searchResult = conn.search("dc=example,dc=com", SearchScope.SUB, "(uid=mue)");
+		System.out.println(searchResult.getEntryCount() + " entries returned.");
+		for (SearchResultEntry e : searchResult.getSearchEntries()) {
+			System.out.println(e.toLDIFString());
+			System.out.println();
+		}	
+	}
+	
+
+	public static void main(String[] args) throws LDAPException {
+
+		//String ldifImport = "/home/mue/work/java/ldapex1/src/main/resources/example1.ldif";
+		String ldifImport = "/home/mue/work/java/ldapex1/src/main/resources/example-com.ldif";
+		
+
+		// Create the configuration to use for the server.
+		InMemoryDirectoryServerConfig config = new InMemoryDirectoryServerConfig("dc=example,dc=com");
+		config.addAdditionalBindCredentials("cn=Manager", "myPassword");
+		
+		// disable schema checking
+		config .setSchema(null);
+		
+		InMemoryListenerConfig listenConf = InMemoryListenerConfig.createLDAPConfig("myLdapListener", LDAP_PORT);
+		
+		//InMemoryListenerConfig listenConf = new InMemoryListenerConfig(ldifImport, null, 0, null, null, null);		
+		config.setListenerConfigs(listenConf);
+
+		// Create the directory server instance, populate it with data from the
+		// "test-data.ldif" file, and start listening for client connections.
+		InMemoryDirectoryServer ds = new InMemoryDirectoryServer(config);
+
+		ds.importFromLDIF(true, ldifImport);
+		System.out.println("loaded ldif file: " + ldifImport);
+		
+		ds.startListening();
+		
+
+		// Get a client connection to the server and use it to perform various
+		// operations.
+		LDAPConnection connInternal = ds.getConnection();
+		System.out.println("using connection: connInternal");
+		doSimpleQuery(connInternal);
+		
+		
+		// create a secured connection trusting any server cert
+		//
+//		SSLUtil sslUtil = new SSLUtil(new TrustAllTrustManager());
+//		SSLSocketFactory socketFactory = sslUtil.createSSLSocketFactory();
+//		LDAPConnection connection = new LDAPConnection(socketFactory,
+//		     "server.example.com", 636);
+				
+		LDAPConnection connExternal = new LDAPConnection("localhost", LDAP_PORT);
+		System.out.println("using connection: connExternal");
+		connExternal.bind("cn=Manager", "myPassword");
+		doSimpleQuery(connExternal);
+
+		
+		
+		connExternal.bind("uid=mue,ou=People,dc=example,dc=com", "myPassword");
+		
+		
+
+		// Disconnect from the server and cause the server to shut down.
+		connExternal.close();
+		connInternal.close();
+		ds.shutDown(true);
+
+	}
+	
+	
+
+}
