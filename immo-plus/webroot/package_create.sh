42ad37ba00ecba25925bf8db093c931a8b8ddd02
mmm <mmm7@bluewin.ch>
2018-07-31 20:42:24 +0200

    Add script package_create

---
+++ immo-plus/webroot/package_create.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "creating package..."
+
+TAR_NAME=iq_homepage.tar.gz
+
+tar cvfz $TAR_NAME img/vcard_mrs_back.png \
+                   img/vcard_mrs_front.png \
+                   img/photo_mrs_profil.jpg \
+                   img/immoQuickLogo.png \
+                   google783f5c3e55fb8f7c.html \
+                   vcard_mrs.html \
+                   iq_sandra_mueller_vcard.vcf \
+                   cdown.html \
+                   iq2.html \
+                   *.ttf \
+                   favicon.png 
+
+scp $TAR_NAME axmue@m1m.ch:/tmp/
