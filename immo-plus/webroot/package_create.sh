7d1abcbd2ed37c8b5e1301484dbae4df168cdf9d
mmm <mmm7@bluewin.ch>
2018-08-25 17:34:08 +0200

    Reformat package_create.sh

--- immo-plus/webroot/package_create.sh
+++ immo-plus/webroot/package_create.sh
@@ -4,18 +4,18 @@ echo "creating package..."
 
 TAR_NAME=iq_homepage.tar.gz
 
-tar cvfz $TAR_NAME img/vcard_mrs_back.png \
-                   img/vcard_mrs_front.png \
-                   img/photo_mrs_profil.jpg \
-                   img/immoQuickLogo.png \
-                   img/piktogrammehaus.png \
-                   img/hurry-1920x1278.jpg \
-                   google783f5c3e55fb8f7c.html \
-                   vcard_mrs.html \
-                   iq_sandra_mueller_vcard.vcf \
-                   cdown.html \
-                   iq2.html \
-                   *.ttf \
+tar cvfz $TAR_NAME img/vcard_mrs_back.png       \
+                   img/vcard_mrs_front.png      \
+                   img/photo_mrs_profil.jpg     \
+                   img/immoQuickLogo.png        \
+                   img/piktogrammehaus.png      \
+                   img/gimp-hurry-1920x1278.png \
+                   google783f5c3e55fb8f7c.html  \
+                   vcard_mrs.html               \
+                   iq_sandra_mueller_vcard.vcf  \
+                   cdown.html                   \
+                   iq2.html                     \
+                   *.ttf                        \
                    favicon.png 
 
 scp $TAR_NAME axmue@m1m.ch:/tmp/
