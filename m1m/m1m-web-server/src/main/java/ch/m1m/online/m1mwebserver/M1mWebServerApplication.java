bd44cd6a72e0103f13c8874dfcebe9076cc3375a
mmm <mmm7@bluewin.ch>
2018-09-30 13:44:46 +0200

    first empty m1m web server

---
+++ m1m/m1m-web-server/src/main/java/ch/m1m/online/m1mwebserver/M1mWebServerApplication.java
@@ -0,0 +1,12 @@
+package ch.m1m.online.m1mwebserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M1mWebServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M1mWebServerApplication.class, args);
+	}
+}
