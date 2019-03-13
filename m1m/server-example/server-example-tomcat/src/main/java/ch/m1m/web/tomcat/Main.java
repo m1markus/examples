8c0bcda2242f5b510b358748be27d6618da6a62d
Markus Müller <markus.mueller@sympany.ch>
2019-03-13 10:19:43 +0100

    QA Add tomcat embedded servlet example

--- m1m/server-example/server-example-tomcat/src/main/java/ch/m1m/web/tomcat/Main.java
+++ m1m/server-example/server-example-tomcat/src/main/java/ch/m1m/web/tomcat/Main.java
@@ -1,10 +1,16 @@
 package ch.m1m.web.tomcat;
 
+import org.apache.catalina.Context;
 import org.apache.catalina.LifecycleException;
 import org.apache.catalina.startup.Tomcat;
 
 import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.File;
+import java.io.IOException;
+import java.io.Writer;
 
 public class Main {
     private static final String STATIC_DIR = "src/main/static/";
@@ -15,6 +21,20 @@ public class Main {
 
         File staticDir = new File(STATIC_DIR);
         tomcat.addWebapp("/", staticDir.getAbsolutePath());
+
+
+        Context ctx = tomcat.addContext("/", new File(".").getAbsolutePath());
+
+        Tomcat.addServlet(ctx, "hello", new HttpServlet() {
+            protected void service(HttpServletRequest req, HttpServletResponse resp)
+                    throws ServletException, IOException {
+                Writer w = resp.getWriter();
+                w.write("Hello, World!");
+                w.flush();
+            }
+        });
+        ctx.addServletMapping("/hello", "hello");
+
 
         tomcat.start();
         tomcat.getServer().await();
