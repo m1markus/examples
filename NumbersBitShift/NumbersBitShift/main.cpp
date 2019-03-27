fd3eba568e9de47775c4c65a929e5b63c68248be
mmm <mmm7@bluewin.ch>
2019-03-27 23:51:55 +0100

    NumbersBitShift Implement digit_8

--- NumbersBitShift/NumbersBitShift/main.cpp
+++ NumbersBitShift/NumbersBitShift/main.cpp
@@ -25,10 +25,29 @@ unsigned char bNumber[] = {
     'H'
 };
 
+// from: https://www.dreamincode.net/forums/topic/195111-how-to-convert-binary-to-decimal-using-c-programming/
+//
+// char c = convert_bit_string_to_byte("1001");
+//
+unsigned char convert_bit_string_to_byte(char *binstring)
+{
+    unsigned char decimalval = 0;
+    int stringpos;
+
+    for (stringpos=(int)strlen(binstring)-1; stringpos>=0; stringpos--) {
+        decimalval = decimalval<<1;
+        if (binstring[stringpos]=='1') decimalval += 1;
+    }
+
+    return decimalval;
+}
 
 // from: https://www.techworld.com.au/article/527677/how_convert_an_ascii_char_binary_string_representation_c/
 //
-void convert_byte_to_string(unsigned char input, unsigned char *output, int outStringSize)
+// unsigned char szBits[8+1];
+// convert_byte_to_bit_string(oneChar, szBits, sizeof(szBits));
+//
+void convert_byte_to_bit_string(unsigned char input, unsigned char *output, int outStringSize)
 {
     int remainder;
 	int BASE=2;
@@ -55,15 +74,36 @@ void print_byte_array_as_bit_string(unsigned char *bNumber, int nSize)
         unsigned char oneChar = bNumber[ii];
         //std::printf("char: %c\n", oneChar);
         //convert_byte_to_string(oneChar, szBits, sizeof(szBits));
-        convert_byte_to_string(oneChar, szBits, sizeof(szBits));
+        convert_byte_to_bit_string(oneChar, szBits, sizeof(szBits));
         std::printf("bit string: %s\n", szBits);
+    }
+}
+
+void print_digit(char digit_8[8][9]) {
+    for (int ii=0; ii<8; ii++) {
+        printf("digit: %s\n", digit_8[ii]);
     }
 }
 
 int main(int argc, const char * argv[]) {
     std::cout << "NumberBitShift running...\n";
     
+    char digit_8[8][9] = {
+        "  ####  ",
+        "  #  #  ",
+        "  #  #  ",
+        "  ####  ",
+        "  #  #  ",
+        "  #  #  ",
+        "  ####  ",
+        "        "
+    };
+    
     print_byte_array_as_bit_string(bNumber, 8);
+    
+    printf("\n");
+    
+    print_digit(digit_8);
     
     std::cout << "### end ###\n";
     return 0;
